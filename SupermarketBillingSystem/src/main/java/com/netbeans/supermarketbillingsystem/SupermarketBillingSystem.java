4f840ac93e26d6f0e53bf8c88db0b440d4d742a9
Mohamme-Dhameem <nmaldhameem115@gmail.com>
2025-11-04 09:02:49 +0530

    Test Commit Initial Setup

---
+++ SupermarketBillingSystem/src/main/java/com/netbeans/supermarketbillingsystem/SupermarketBillingSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.netbeans.supermarketbillingsystem;
+
+/**
+ *
+ * @author mohammed-dhameem
+ */
+public class SupermarketBillingSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
